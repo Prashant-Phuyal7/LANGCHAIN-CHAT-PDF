090268928de2276028d5316939bd4ded03dbd83f
Prashant-Phuyal7 <prashantphuyal7@gmail.com>
2025-09-16 17:34:18 +0545

    fix:changes

---
+++ app.py
@@ -0,0 +1,60 @@
+from dotenv import load_dotenv
+import streamlit as st
+from PyPDF2 import PdfReader
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.vectorstores import FAISS
+from langchain.chains.question_answering import load_qa_chain
+from langchain.llms import OpenAI
+from langchain.callbacks import get_openai_callback
+
+def main():
+    load_dotenv()
+    st.set_page_config(page_title='Ask your PDF')
+    st.header('Ask your pdf') 
+       
+    # upload file
+    pdf = st.file_uploader("upload your pdf", type="pdf")
+    
+    # extract the text
+    if pdf is not None:
+        pdf_reader = PdfReader(pdf)
+        text =""
+        for page in pdf_reader.pages:
+            text += page.extract_text()
+            
+        
+        # split into chunks
+        text_splitter = CharacterTextSplitter(
+             separator="\n",
+             chunk_size = 500,
+             chunk_overlap = 100,
+             length_function = len
+        )
+        
+         
+        chunks = text_splitter.split_text(text)
+         
+        # create embedings 
+        embeddings = OpenAIEmbeddings()
+        knowledge_base = FAISS.from_texts(chunks, embeddings)
+        
+        # show user input
+        user_question = st.text_input("Ask a question about your pdf:")
+        if user_question:
+            docs = knowledge_base.similarity_search(user_question)
+            
+            llm = OpenaAI()
+            chain = load_qa_chain(llm, chain_type="stuff")
+            with get_openai_callback() as cb:
+             response = chain.run(input_documents=docs, question=user_question)
+             print(cb)
+            st.write(response)
+        
+        
+            
+        
+
+
+if __name__ == '__main__':
+    main()
